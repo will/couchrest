d5b9e86cc94e45affa2f19b7d2951cb0cfa3db3d
Will Leinweber <will@bitfission.com>
2009-09-27 15:15:43 -0500

    start protected flag

--- spec/couchrest/more/attribute_protection_spec.rb
+++ spec/couchrest/more/attribute_protection_spec.rb
@@ -1,21 +1,67 @@
 require File.expand_path("../../../spec_helper", __FILE__)
 
-describe "ExtendedDocument" do
-  class WithAccessable< CouchRest::ExtendedDocument
+describe "ExtendedDocument", "no declerations" do
+  class NoProtection < CouchRest::ExtendedDocument
+    use_database TEST_SERVER.default_database
+		property :name
+		property :phone
+	end
+
+  it "should not protect anything through new" do
+    user = NoProtection.new(:name => "will", :phone => "555-5555")
+
+		user.name.should == "will"
+		user.phone.should == "555-5555"
+	end
+
+  it "should not protect anything through attributes=" do
+    user = NoProtection.new
+		user.attributes = {:name => "will", :phone => "555-5555"}
+
+		user.name.should == "will"
+		user.phone.should == "555-5555"
+	end
+end
+
+describe "ExtendedDocument", "accessable flag" do
+  class WithAccessable < CouchRest::ExtendedDocument
     use_database TEST_SERVER.default_database
 		property :name, :accessable => true
 		property :admin, :default => false
 	end
 
-	it "should not allow non accessable set through new" do
+	it "should protect non-accessable properties set through new" do
     user = WithAccessable.new(:name => "will", :admin => true) 
 
 		user.name.should == "will"
 		user.admin.should == false
 	end
 
-	it "should not allow non accessable set through attributes=" do
+	it "should protect non-accessable properties set through attributes=" do
     user = WithAccessable.new
+		user.attributes = {:name => "will", :admin => true}
+
+		user.name.should == "will"
+		user.admin.should == false
+	end
+end
+
+describe "ExtendedDocument", "protected flag" do
+  class WithProtected < CouchRest::ExtendedDocument
+    use_database TEST_SERVER.default_database
+		property :name
+		property :admin, :default => false, :protected => true
+	end
+
+	it "should protect non-accessable properties set through new" do
+    user = WithProtected.new(:name => "will", :admin => true) 
+
+		user.name.should == "will"
+		user.admin.should == false
+	end
+
+	it "should protect non-accessable properties set through attributes=" do
+    user = WithProtected.new
 		user.attributes = {:name => "will", :admin => true}
 
 		user.name.should == "will"
