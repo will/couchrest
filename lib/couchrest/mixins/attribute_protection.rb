dc675ec2d5a3463b5baa616cc7808972e993d228
Will Leinweber <will@bitfission.com>
2009-09-27 18:56:07 -0500

    documentation and spelling fixes

--- lib/couchrest/mixins/attribute_protection.rb
+++ lib/couchrest/mixins/attribute_protection.rb
@@ -1,55 +1,74 @@
 module CouchRest
-	module Mixins
-		module AttributeProtection
-	    def self.included(base)
-			  base.extend(ClassMethods)
-			end
+  module Mixins
+    module AttributeProtection
+      # Attribute protection from mass assignment to CouchRest properties
+      # 
+      # Protected methods will be removed from
+      #  * new 
+      #  * update_attributes
+      #  * upate_attributes_without_saving
+      #  * attributes=
+      #  
+      # There are two modes of protection
+      #  1) Declare accessible poperties, assume all the rest are protected 
+      #    property :name, :accessible => true
+      #    property :admin                     # this will be automatically protected
+      #
+      #  2) Declare protected properties, assume all the rest are accessible
+      #    property :name                      # this will not be protected
+      #    property :admin, :protected => true
+      #
+      # Note: you cannot set both flags in a single class
+      
+      def self.included(base)
+        base.extend(ClassMethods)
+      end
 
-			module ClassMethods
-			  def accessable_properties
-          properties.select { |prop| prop.options[:accessable] }
-				end
+      module ClassMethods
+        def accessible_properties
+          properties.select { |prop| prop.options[:accessible] }
+        end
 
-				def protected_properties
+        def protected_properties
           properties.select { |prop| prop.options[:protected] }
-				end
-			end
+        end
+      end
 
-      def accessable_properties
-			  self.class.accessable_properties
-			end
+      def accessible_properties
+        self.class.accessible_properties
+      end
 
       def protected_properties
-			  self.class.protected_properties
-			end
+        self.class.protected_properties
+      end
 
       def remove_protected_attributes(attributes)
-				protected_names = properties_to_remove_from_mass_assignment.map { |prop| prop.name }
-				return attributes if protected_names.empty?
+        protected_names = properties_to_remove_from_mass_assignment.map { |prop| prop.name }
+        return attributes if protected_names.empty?
 
-			  attributes.reject! do |key, value|
+        attributes.reject! do |key, value|
           protected_names.include?(key.to_s)
-				end
+        end
 
-				attributes || {}
-			end
+        attributes || {}
+      end
 
-			private
+      private
 
-			def properties_to_remove_from_mass_assignment
-				has_protected = !protected_properties.empty?
-				has_accessable = !accessable_properties.empty?
+      def properties_to_remove_from_mass_assignment
+        has_protected = !protected_properties.empty?
+        has_accessible = !accessible_properties.empty?
 
-				if !has_protected && !has_accessable
-					[]
-				elsif has_protected && !has_accessable
-					protected_properties
-				elsif has_accessable && !has_protected
-					properties.reject { |prop| prop.options[:accessable] }
-				else
-					raise "Set either :accessable or :protected for #{self.class}, but not both"
-				end
-			end
-		end
-	end
+        if !has_protected && !has_accessible
+          []
+        elsif has_protected && !has_accessible
+          protected_properties
+        elsif has_accessible && !has_protected
+          properties.reject { |prop| prop.options[:accessible] }
+        else
+          raise "Set either :accessible or :protected for #{self.class}, but not both"
+        end
+      end
+    end
+  end
 end
--- spec/couchrest/more/attribute_protection_spec.rb
+++ spec/couchrest/more/attribute_protection_spec.rb
@@ -1,94 +1,94 @@
 require File.expand_path("../../../spec_helper", __FILE__)
 
-describe "ExtendedDocument", "no declerations" do
+describe "ExtendedDocument", "no declarations" do
   class NoProtection < CouchRest::ExtendedDocument
     use_database TEST_SERVER.default_database
-		property :name
-		property :phone
-	end
+    property :name
+    property :phone
+  end
 
   it "should not protect anything through new" do
     user = NoProtection.new(:name => "will", :phone => "555-5555")
 
-		user.name.should == "will"
-		user.phone.should == "555-5555"
-	end
+    user.name.should == "will"
+    user.phone.should == "555-5555"
+  end
 
   it "should not protect anything through attributes=" do
     user = NoProtection.new
-		user.attributes = {:name => "will", :phone => "555-5555"}
+    user.attributes = {:name => "will", :phone => "555-5555"}
 
-		user.name.should == "will"
-		user.phone.should == "555-5555"
-	end
+    user.name.should == "will"
+    user.phone.should == "555-5555"
+  end
 end
 
-describe "ExtendedDocument", "accessable flag" do
-  class WithAccessable < CouchRest::ExtendedDocument
+describe "ExtendedDocument", "accessible flag" do
+  class Withaccessible < CouchRest::ExtendedDocument
     use_database TEST_SERVER.default_database
-		property :name, :accessable => true
-		property :admin, :default => false
-	end
+    property :name, :accessible => true
+    property :admin, :default => false
+  end
 
-	it "should recoginze accessable properties" do
-    props = WithAccessable.accessable_properties.map { |prop| prop.name}
-		props.should include("name")
-		props.should_not include("admin")
-	end
+  it "should recognize accessible properties" do
+    props = Withaccessible.accessible_properties.map { |prop| prop.name}
+    props.should include("name")
+    props.should_not include("admin")
+  end
 
-	it "should protect non-accessable properties set through new" do
-    user = WithAccessable.new(:name => "will", :admin => true) 
+  it "should protect non-accessible properties set through new" do
+    user = Withaccessible.new(:name => "will", :admin => true) 
 
-		user.name.should == "will"
-		user.admin.should == false
-	end
+    user.name.should == "will"
+    user.admin.should == false
+  end
 
-	it "should protect non-accessable properties set through attributes=" do
-    user = WithAccessable.new
-		user.attributes = {:name => "will", :admin => true}
+  it "should protect non-accessible properties set through attributes=" do
+    user = Withaccessible.new
+    user.attributes = {:name => "will", :admin => true}
 
-		user.name.should == "will"
-		user.admin.should == false
-	end
+    user.name.should == "will"
+    user.admin.should == false
+  end
 end
 
 describe "ExtendedDocument", "protected flag" do
   class WithProtected < CouchRest::ExtendedDocument
     use_database TEST_SERVER.default_database
-		property :name
-		property :admin, :default => false, :protected => true
-	end
+    property :name
+    property :admin, :default => false, :protected => true
+  end
 
-	it "should recoginze protected properties" do
+  it "should recognize protected properties" do
     props = WithProtected.protected_properties.map { |prop| prop.name}
-		props.should_not include("name")
-		props.should include("admin")
-	end
+    props.should_not include("name")
+    props.should include("admin")
+  end
 
-	it "should protect non-accessable properties set through new" do
+  it "should protect non-accessible properties set through new" do
     user = WithProtected.new(:name => "will", :admin => true) 
 
-		user.name.should == "will"
-		user.admin.should == false
-	end
+    user.name.should == "will"
+    user.admin.should == false
+  end
 
-	it "should protect non-accessable properties set through attributes=" do
+  it "should protect non-accessible properties set through attributes=" do
     user = WithProtected.new
-		user.attributes = {:name => "will", :admin => true}
+    user.attributes = {:name => "will", :admin => true}
 
-		user.name.should == "will"
-		user.admin.should == false
-	end
+    user.name.should == "will"
+    user.admin.should == false
+  end
 end
 
 describe "ExtendedDocument", "protected flag" do
   class WithBoth < CouchRest::ExtendedDocument
     use_database TEST_SERVER.default_database
-		property :name, :accessable => true
-		property :admin, :default => false, :protected => true
-	end
+    property :name, :accessible => true
+    property :admin, :default => false, :protected => true
+  end
 
-	it "should raise an error when both are set" do
-    expect { WithBoth.new }.to raise_error 
-	end
+  it "should raise an error when both are set" do
+    lambda { WithBoth.new }.should raise_error 
+  end
 end
